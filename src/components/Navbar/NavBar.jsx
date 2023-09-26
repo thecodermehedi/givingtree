9c40f8a0413570346a71ba364d09ae1b3b57c0e1
Mehedi Hasan <devbytemehedi@gmail.com>
2023-09-26 22:52:59 +0600

    update NavBar.jsx

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,10 +1,13 @@
+import {Link} from "react-router-dom";
 import NavLinks from "../NavLinks/NavLinks";
 import NavMobile from "../NavMobile/NavMobile";
 import Logo from "./../Logo/Logo";
 const NavBar = () => {
   return (
     <div className="flex justify-between items-center p-5 md:p-10 lg:px-20">
-      <Logo width={"md:w-64"} />
+      <Link to={"https://givingtree.vercel.app"}>
+        <Logo width={"md:w-64"} />
+      </Link>
       <NavLinks />
       <NavMobile />
     </div>
