e76d9fd0d73a7a8c070ab1ec48365a6aaf104fe4
Mehedi Hasan <devbytemehedi@gmail.com>
2023-09-26 21:20:10 +0600

    update HomePage.jsx

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,7 +1,34 @@
+import {useState, useEffect} from "react";
+import Banner from "../../components/Banner/Banner";
+import Cards from "../../components/Cards/Cards";
 const Home = () => {
+  const [cards, setCards] = useState([]);
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setCards(data);
+        setNewCards(data);
+      });
+  }, []);
+  const [newCards, setNewCards] = useState([]);
+  const [query, setQuery] = useState("");
+  const handleSearch = (query) => {
+    if (newCards.length > 0 && query.length > 0) {
+      const queryCards = cards.filter((card) =>
+        card.category.toLowerCase().includes(query.toLowerCase())
+      );
+      setNewCards(queryCards);
+      setQuery("");
+    } else if (query === "") {
+      setNewCards(cards);
+      setQuery("");
+    }
+  };
   return (
     <div>
-      <h1>Homepage dynamic part</h1>
+      <Banner setQuery={setQuery} query={query} handleSearch={handleSearch} />
+      <Cards newCards={newCards} />
     </div>
   );
 };
