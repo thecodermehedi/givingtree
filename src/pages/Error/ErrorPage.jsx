8ddba1c3a61a05709f758857de8104bf573c09c6
Mehedi Hasan <devbytemehedi@gmail.com>
2023-09-26 21:20:05 +0600

    update ErrorPage.jsx

--- src/pages/Error/ErrorPage.jsx
+++ src/pages/Error/ErrorPage.jsx
@@ -1,8 +1,28 @@
+import {useRouteError} from "react-router-dom";
 const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
   return (
-    <div>
-      <h1>ErrorPage</h1>
-    </div>
+    <>
+      {error.status === 404 ? (
+        <div className="h-screen flex flex-col justify-center items-center relative font-montserrat">
+          <h1 className="font-bold uppercase text-sm text-black absolute top-64">
+            Oops! page not found
+          </h1>
+          <h1 className="font-black text-black text-[10rem] tracking-tighter">
+            {error.status}
+          </h1>
+          <p className="font-semibold uppercase text-sm text-black absolute bottom-56 text-center">
+            we are sorry, but the page you requested was <br /> not found
+          </p>
+        </div>
+      ) : (
+        <h1 className="font-bold uppercase text-sm text-black">
+          {error.status}
+        </h1>
+      )}
+    </>
   );
 };
 
