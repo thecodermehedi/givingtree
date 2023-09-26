d056ade3e6ae84cbfbbead0053d810f80b2c9ecc
Mehedi Hasan <devbytemehedi@gmail.com>
2023-09-26 22:52:55 +0600

    update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import Logo from "../Logo/Logo";
 import SocialIcons from "../SocialIcons/SocialIcons";
+import {Link} from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="flex flex-col gap-5 lg:flex-row justify-between items-center py-10 px-20">
-      <Logo width={"w-64"} />
+      <Link to={"https://givingtree.vercel.app"}>
+        <Logo width={"md:w-64"} />
+      </Link>
       <p className="font-bold opacity-90">
         Copyright © 2023{" "}
         <span className="hidden md:inline">- All rights reserved</span>
