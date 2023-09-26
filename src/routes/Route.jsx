4acf7ec350f534e56f7706c1d91fbb6a158887e2
Mehedi Hasan <devbytemehedi@gmail.com>
2023-09-26 21:20:19 +0600

    update Route.jsx

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -2,6 +2,9 @@ import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import HomePage from "../pages/Home/HomePage";
 import ErrorPage from "../pages/Error/ErrorPage";
+import Donation from "../pages/Donation/Donation";
+import Statistics from "../pages/Statistics/Statistics";
+import DonationDetails from "../pages/DonationDetails/DonationDetails";
 
 const Route = createBrowserRouter([
   {
@@ -12,6 +15,21 @@ const Route = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
+      },
+      {
+        path: "/donation-details/:id",
+        element: <DonationDetails />,
+        loader: () => fetch("/data.json"),
+      },
+      {
+        path: "/donation",
+        element: <Donation />,
+        loader: () => fetch("/data.json"),
+      },
+      {
+        path: "/statistics",
+        element: <Statistics />,
+        loader: () => fetch("/data.json"),
       },
     ],
   },
