4db201065a8fad17f229caaf377d679b8a8c4575
Mehedi Hasan <devbytemehedi@gmail.com>
2023-09-26 21:19:50 +0600

    update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,13 @@
 import {Outlet} from "react-router-dom";
+import NavBar from "../components/Navbar/NavBar";
+import Footer from "../components/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
+      <NavBar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
